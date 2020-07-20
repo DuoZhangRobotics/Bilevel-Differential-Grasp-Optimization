87cefff557384caf16b46f08bb61c2d30f2e5554
kraftoreo <976539567@qq.com>
2020-07-20 15:43:06 +0800

    finite difference check

--- commit_kraftoreo.sh
+++ commit_kraftoreo.sh
@@ -1,5 +1,14 @@
-rm -r ./__pycache__
-rm -r ./limits
+# check if directory ./limist exists. if so, delete it
+if [ -d "./limits"]
+then
+	rm -r ./limits
+fi
+
+# check if directory ./__pycache exists. if so, delete it
+if [ -d "./__pycache__"]
+then
+	rm -r ./__pycache__
+fi
 git add --all
 git commit -m "$1"
 git push -u origin master
