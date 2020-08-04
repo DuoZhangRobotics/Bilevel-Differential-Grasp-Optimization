SGD method implemented, but wrong result

HandTarget.py
@@ -1,6 +1,6 @@
 import numpy as np
 import vtk
-from hand import Hand, vtk_render, vtk_add_from_hand, Link
+from Hand import Hand, vtk_render, vtk_add_from_hand, Link
 import torch
 import trimesh
 
@@ -26,7 +26,8 @@ class HandTarget(object):
         self.rotation_matrix = torch.eye(3, dtype=data_type).repeat((1, self.hand.link_num))
         self._initialize_params(self.root, self.target, 0)
         self.chart_reset(self.root, 0)
-        self.params.requires_grad_(True)
+        self.params = self.params.detach().clone().requires_grad_(True)
+        print(self.params)
 
     def _initialize_params(self, root: Link, target: trimesh.Trimesh, start):
         centroid0 = root.centroid
@@ -44,7 +45,8 @@ class HandTarget(object):
         self.params[0, self.front + 3 * start + 2] = d
         start += 1
         for child in root.children:
-            self._initialize_params(child, target, start)
+            start = self._initialize_params(child, target, start)
+        return start
 
     def chart_reset(self, root: Link, start):
         beta, phi = self.get_beta_phi(start)
@@ -60,18 +62,25 @@ class HandTarget(object):
                               ], dtype=data_type)
         self.params[0, self.front + 3 * start].data.zero_()
         self.params[0, self.front + 3 * start + 1].data.zero_()
-        self.rotation_matrix[:, start: start + 3] @= rot_z @ rot_y
+        self.rotation_matrix[:, start: start + 3] @= rot_z @ rot_y.clone()
         start += 1
         for child in root.children:
-            self.chart_reset(child, start)
+            start = self.chart_reset(child, start)
+        return start
 
     def _initialize_d(self, root: Link, target: trimesh.Trimesh, start):
         v0, v2 = torch.tensor(root.mesh.vertices, dtype=data_type), torch.tensor(target.vertices, dtype=data_type)
         beta, phi = self.get_beta_phi(start)
         n = self.get_n(self.rotation_matrix[:, start: start + 3], beta, phi)
-        lower_bound = torch.min(v2 @ n)
+        lower_bound2 = torch.min(v2 @ n)
+        upper_bound2 = torch.max(v2 @ n)
         upper_bound = torch.max(v0 @ n)
-        d = torch.mean(torch.tensor([lower_bound, upper_bound]))
+        lower_bound = torch.min(v0 @ n)
+        d = None
+        if lower_bound > upper_bound2:
+            d = torch.mean(torch.tensor([lower_bound, upper_bound2]))
+        if lower_bound2 > upper_bound:
+            d = torch.mean(torch.tensor([lower_bound2, upper_bound]))
         return d.detach().clone()
 
     @staticmethod

LineSearcher.py
@@ -76,15 +76,13 @@ class LineSearcher(object):
 
         # line search using Armijo Condition
         # Pre-calculation for Armijo condition, namely, the product of first order derivative and line searching
-
-
         tmp_output, _ = self.update_output(s, direction)
         while tmp_output > output - c1 * s * grad @ direction or torch.isnan(tmp_output):
             s *= scale
             tmp_output, _ = self.update_output(s, direction)
             if s <= tol:
                 print("Step size is too small. Line search terminated when testing the armijo condition")
-                break
+                return None
         if mode == 'Wolfe':
             pass
         return s
@@ -98,3 +96,4 @@ class LineSearcher(object):
         params = self.params
         params[0] = param0
         return params
+

Objectives.py
@@ -1,6 +1,6 @@
 import torch
 import numpy as np
-from hand import Link, Hand
+from Hand import Link, Hand
 import trimesh
 from ConvexhullSettings import ConvexHullSettings
 from HandTarget import HandTarget
@@ -35,38 +35,47 @@ def obj_fun(params, ch_settings: ConvexHullSettings, gamma=torch.tensor(0.001, d
 def hand_obj_fun(params, hand_target: HandTarget, gamma=torch.tensor(0.01, dtype=torch.double)):
     motion_params = params[:, :hand_target.front]
     p, t = hand_target.hand.forward(motion_params)
-    norm = get_norm(hand_target.hand.palm, hand_target.target, p, 0)
-    objective = gamma * get_constrain(hand_target.hand.palm, hand_target.target, hand_target, p, 0, 0)
-    print(objective)
-    print(norm)
-    objective += norm
-    return norm
+    norm, _ = get_norm(hand_target.hand.palm, hand_target.target, p, 0)
+    objective, _, _ = get_constrain(hand_target.hand.palm, hand_target.target, hand_target, p, 0, 0)
+    objective = objective * gamma
+    objective = objective + norm
+    return objective
 
 
 def get_norm(root: Link, target_link: trimesh.Trimesh, p, start=0):
     centroid1 = torch.tensor(target_link.centroid, dtype=torch.double)
     centroid0 = torch.mean(p[:, :, start: start + len(root.mesh.vertices)], dim=2)
-    print(centroid0, centroid1)
     norm = torch.norm(centroid0 - centroid1)
     start += len(root.mesh.vertices)
     if root.children:
         for child in root.children:
-            norm += get_norm(child, target_link, p, start)
-    return norm
+            tmp_norm, start = get_norm(child, target_link, p, start)
+            norm = norm + tmp_norm
+    return norm, start
 
 
 def get_constrain(root: Link, target_link: trimesh.Trimesh, hand_target: HandTarget, p, start, idx):
+    objective = None
     v1 = p[:, :, start: start + len(root.mesh.vertices)].view(3, -1).T
     v2 = torch.tensor(target_link.vertices, dtype=torch.double)
     rotation_matrix = hand_target.get_rotation_matrix(idx)
     beta, phi = hand_target.get_beta_phi(idx)
     d = hand_target.get_d(idx)
     n = get_n(rotation_matrix, beta, phi)
-    objective = -torch.sum(torch.log(v1 @ n - d)) - torch.sum(torch.log(d - v2 @ n))
+    lower_bound2 = torch.min(v2 @ n)
+    upper_bound2 = torch.max(v2 @ n)
+    upper_bound = torch.max(v1 @ n)
+    lower_bound = torch.min(v1 @ n)
+    if lower_bound > upper_bound2:
+        objective = -torch.sum(torch.log(v1 @ n - d)) - torch.sum(torch.log(d - v2 @ n))
+    elif lower_bound2 > upper_bound:
+        objective = -torch.sum(torch.log(v2 @ n - d)) - torch.sum(torch.log(d - v1 @ n))
+    else:
+        objective = -torch.sum(torch.log(v2 @ n - d)) - torch.sum(torch.log(d - v1 @ n))
 
     start += len(root.mesh.vertices)
     idx += 1
     for child in root.children:
-        objective += get_constrain(child, target_link, hand_target, p, start, idx)
-
-    return objective
+        tmp_objective, start, idx = get_constrain(child, target_link, hand_target, p, start, idx)
+        objective = objective + tmp_objective
+    return objective, start, idx

Optimizer.py
@@ -4,6 +4,8 @@ from typing import Callable
 import numpy as np
 from numpy.linalg import LinAlgError
 from HandTarget import HandTarget
+from torchviz import make_dot, make_dot_from_trace
+
 # define pi in torch
 pi = torch.acos(torch.zeros(1)).item() * 2
 data_type = torch.double
@@ -19,15 +21,16 @@ class Optimizer(object):
         print(f"OUTPUT = {self.output}")
         self.params = params
         self.line_searcher = LineSearcher(self.func, self.params)
-        self.params_need_to_be_optimized = self.params[0]
         self.objectives = []
         self.method = method
         self.mode = mode
         self.jacobian_matrix: torch.tensor = torch.autograd.grad(self.output,
-                                                                 self.params_need_to_be_optimized,
+                                                                 self.params[0],
                                                                  retain_graph=True,
                                                                  create_graph=True)[0]
-        self.hessian_matrix = self.hessian()
+        # self.grad_check()
+        if self.method == "Newton":
+            self.hessian_matrix = self.hessian()
         self.direction = self.find_direction()
         self.c1 = c1
         self.c2 = c2
@@ -38,11 +41,8 @@ class Optimizer(object):
         for i in range(niters):
             self.objectives.append(self.output)
             grad = self.jacobian_matrix.reshape((1, -1))
-            line_searching_direction = grad.T
-            if self.method == 'Newton':
-                line_searching_direction = torch.inverse(self.hessian_matrix) @ line_searching_direction
             self.s = self.line_searcher.line_search(grad=grad,
-                                                    direction=line_searching_direction,
+                                                    direction=self.direction,
                                                     output=self.output,
                                                     mode=self.mode,
                                                     tol=tol,
@@ -50,24 +50,30 @@ class Optimizer(object):
                                                     c1=self.c1,
                                                     c2=self.c2,
                                                     s=self.s)
-
-            self.reset_parameters()
-            self.s *= invscale
-            print(f"iter {i + 1}", self.jacobian_matrix)
-            if self.grad_norm() < tolg:
-                print("Converged!")
+            if self.s is not None:
+                self.reset_parameters()
+                self.s *= invscale
+                print(f"iter {i + 1}", self.jacobian_matrix)
+                if self.grad_norm() < tolg:
+                    print("Converged!")
+                    break
+            else:
                 break
 
     def hessian(self):
         length = self.jacobian_matrix.size(1)
         hessian_matrix = torch.zeros((length, length), dtype=data_type)
         for i in range(length):
-            hessian_matrix[i, :] = torch.autograd.grad(self.jacobian_matrix[0, i], self.params[0], retain_graph=True)[0]
+            # with torch.autograd.detect_anomaly():
+            hessian_matrix[i, :] = torch.autograd.grad(self.jacobian_matrix[0, i],
+                                                       self.params[0],
+                                                       retain_graph=True)[0]
+            print(hessian_matrix[i, :])
         # check if the hessian matrix is positive definite
         try:
             np.linalg.cholesky(hessian_matrix.detach().numpy())
         except LinAlgError:
-            # if the hessian matrix is not positive definite, then make it positive definite.
+            # if the hessian matrix is not positive definite, ¬then make it positive definite.
             print("The Hessian matrix is not positive definite. Now trying to correct it...")
             hessian_matrix = torch.tensor(self.make_it_positive_definite(), dtype=data_type)
         return hessian_matrix
@@ -97,16 +103,14 @@ class Optimizer(object):
         return direction
 
     def reset_parameters(self):
-        param0 = self.params_need_to_be_optimized - self.s * self.direction.T
-        # self.params[0] = param0
+        param0 = self.params[0] - self.s * self.direction.T
         self.hand_target.reset_parameters(param0)
         self.params[0] = self.hand_target.params
+        self._reinit()
 
-    def get_params(self):
-        beta =  self.params_need_to_be_optimized[0, 0]
-        phi =   self.params_need_to_be_optimized[0, 1]
-        theta = self.params_need_to_be_optimized[0, 2: 5]
-        d =     self.params_need_to_be_optimized[0, 5]
-        
+    def _reinit(self):
+        self.__init__(self.hand_target, self.func, self.params, self.method, self.mode, self.c1, self.c2,
+                      self.s, self.gamma)
 
-
+    def grad_check(self):
+        print(torch.autograd.gradcheck(self.func, self.params))

main.py
@@ -7,11 +7,13 @@ from Objectives import obj_fun, hand_obj_fun
 from Optimizer import Optimizer
 from Plot import Plot
 import trimesh
-from hand import Hand, Link
+from Hand import Hand, Link, vtk_render, vtk_add_from_hand
 from HandTarget import HandTarget
+import vtk
 # define pi in torch
 pi = torch.acos(torch.zeros(1)).item() * 2
 data_type = torch.double
+
 
 if __name__ == "__main__":
     path = r'./cube/'
@@ -21,10 +23,10 @@ if __name__ == "__main__":
     hand = Hand(path, scale, use_joint_limit=True, use_quat=True, use_eigen=use_eigen)
     if hand.use_eigen:
         dofs = np.zeros(hand.eg_num)
-        params = 100 * torch.ones((1, hand.extrinsic_size + hand.eg_num))
+        params = 100 * torch.zeros((1, hand.extrinsic_size + hand.eg_num))
     else:
         dofs = np.zeros(hand.nr_dof())
-        params = 100 * torch.ones((1, hand.extrinsic_size + hand.nr_dof()))
+        params = 100 * torch.zeros((1, hand.extrinsic_size + hand.nr_dof()))
     hand.forward(params)
     cube = np.array([[-0.5, -0.5, -0.5],
                      [-0.5, 0.5, -0.5],
@@ -42,8 +44,15 @@ if __name__ == "__main__":
 
     gamma = torch.tensor(0.01, dtype=data_type)
     hand_target = HandTarget(hand, target)
-    optimizer = Optimizer(hand_target, hand_obj_fun, params=[hand_target.params, hand_target, gamma], mode='Armijo', method='Newton')
+    optimizer = Optimizer(hand_target, hand_obj_fun, params=[hand_target.params, hand_target, gamma], mode='Armijo', method='SGD')
     optimizer.optimize(niters=100)
+    print(params)
+    params = optimizer.params[0][:, :hand_target.front].detach()
+    print('params = ', params)
+    hand.forward(params)
+    renderer = vtk.vtkRenderer()
+    vtk_add_from_hand(hand, renderer, 1.0, use_torch=True)
+    vtk_render(renderer, axes=False)
     # plotter = Plot(bioptimizer)
     # plotter.plot_convex_hulls()
     # plotter.plot_obj()

test.py
@@ -1,9 +1,8 @@
 import numpy as np
 import vtk
-from hand import Hand, vtk_render, vtk_add_from_hand, Link
+from Hand import Hand, vtk_render, vtk_add_from_hand, Link
 import torch
 import trimesh
-
 
 
 path = r'./cube/'
@@ -17,11 +16,7 @@ else:
     dofs = np.zeros(hand.nr_dof())
     params = 100 * torch.zeros((1, hand.extrinsic_size + hand.nr_dof()))
 p, t = hand.forward(params)
-print(hand.palm.mesh.vertices)
-print(p[:, :, :len(hand.palm.mesh.vertices)])
-print(p.shape)
-print('====')
-print(t.shape)
+
 # hand.forward_kinematics(np.zeros(hand.extrinsic_size), dofs)
 # hand.write_limits()
 renderer = vtk.vtkRenderer()