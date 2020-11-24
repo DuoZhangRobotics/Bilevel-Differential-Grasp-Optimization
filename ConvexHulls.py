5420ff27e0011d04170099a686f925f4016856a7
kraftoreo <976539567@qq.com>
2020-11-24 20:50:46 +0800

    sync

--- ConvexHulls.py
+++ ConvexHulls.py
@@ -4,6 +4,7 @@ import cvxpy as cp
 
 data_type = torch.double
 
+
 class ConvexHull(object):
     def __init__(self, points: np.array):
         self.points = points
@@ -17,8 +18,8 @@ class ConvexHull(object):
     def distance_to(self, target_hull):
         # distance between two convex hulls
         x1 = cp.Variable(3)
-        
-        if isinstance(target_hull,ConvexHull):
+
+        if isinstance(target_hull, ConvexHull):
             x2 = cp.Variable(3)
             A2, b2 = target_hull.A, target_hull.b
             constraints = [self.A @ x1 <= self.b, A2 @ x2 <= b2]
@@ -29,11 +30,12 @@ class ConvexHull(object):
         objective = cp.Minimize(cp.sum_squares(x1 - x2))
         prob = cp.Problem(objective, constraints)
         min_dist = prob.solve(solver=cp.MOSEK)
-        
-        if isinstance(target_hull,ConvexHull):
+
+        if isinstance(target_hull, ConvexHull):
             return min_dist, x1.value, x2.value
-        else: return min_dist, x1.value, x2
-    
+        else:
+            return min_dist, x1.value, x2
+
     def surface_vertices(self):
         return self.points[self.vertices, :]
 
@@ -41,9 +43,10 @@ class ConvexHull(object):
         faces = []
         for f in self.simplices:
             if f[0] in self.vertices and f[1] in self.vertices and f[2] in self.vertices:
-                faces.append([self.vertices.tolist().index(f[0]),self.vertices.tolist().index(f[1]),self.vertices.tolist().index(f[2])])
+                faces.append([self.vertices.tolist().index(f[0]), self.vertices.tolist().index(f[1]),
+                              self.vertices.tolist().index(f[2])])
         return np.array(faces)
-    
+
     def mesh(self):
         return trimesh.Trimesh(vertices=self.surface_vertices(), faces=self.surface_indices())
 
--- Metric.py
+++ Metric.py
@@ -3,6 +3,7 @@ from Hand import Hand
 import scipy, trimesh
 import numpy as np
 
+
 class Metric(object):
     def __init__(self, targets, count=100, friCoef=.7, res=3, M=None):
         self.points = []
@@ -12,69 +13,70 @@ class Metric(object):
         samples = [trimesh.sample.sample_surface_even(t, count) for t in self.meshes]
         for tid, sample in enumerate(samples):
             for sid in range(sample[0].shape[0]):
-                pt = sample[0][sid,:].tolist()
+                pt = sample[0][sid, :].tolist()
                 fid = sample[1][sid]
-                contain = False 
+                contain = False
                 for tid2, target in enumerate(targets):
-                    if tid2!=tid and target.contain(pt):
-                        contain=True
+                    if tid2 != tid and target.contain(pt):
+                        contain = True
                         break
                 if not contain:
                     self.points.append(pt)
                     self.normals.append(-self.meshes[tid].face_normals[fid])
         self.mu = friCoef
-        self.points=np.array(self.points,dtype=np.float64).T
-        self.normals=np.array(self.normals,dtype=np.float64).T
-        
-        #sample directions
+        self.points = np.array(self.points, dtype=np.float64).T
+        self.normals = np.array(self.normals, dtype=np.float64).T
+
+        # sample directions
         from Directions import Directions
         self.dirs = Directions(res=res, dim=6).dirs
-        self.dirs=np.array(self.dirs,dtype=np.float64).T
-        
-        #compute gij
-        self.gij = np.zeros((self.points.shape[1],self.dirs.shape[1]), dtype=np.float64)
+        self.dirs = np.array(self.dirs, dtype=np.float64).T
+
+        # compute gij
+        self.gij = np.zeros((self.points.shape[1], self.dirs.shape[1]), dtype=np.float64)
         for i in range(self.gij.shape[0]):
-            p=self.points[:,i]
-            n=self.normals[:,i]
+            p = self.points[:, i]
+            n = self.normals[:, i]
             for j in range(self.gij.shape[1]):
-                d=self.dirs[:,j]
-                f2w=np.concatenate((np.eye(3,3,dtype=np.float64),Metric.cross(p)))
+                d = self.dirs[:, j]
+                f2w = np.concatenate((np.eye(3, 3, dtype=np.float64), Metric.cross(p)))
                 if M is not None:
-                    f2w=np.matmul(M,f2w)
-                w_perp=(np.mat(d)*f2w*np.mat(n).T)[0,0]
-                w_para=np.linalg.norm(np.mat(d)*f2w*(np.identity(3)-np.mat(n).T*np.mat(n)))
-                if self.mu*w_perp>w_para:
-                    max_sw=(w_perp+w_para**2/w_perp)
-                else: max_sw=max(0,w_perp+self.mu*w_para)
-                self.gij[i][j]=max_sw
-                
+                    f2w = np.matmul(M, f2w)
+                w_perp = (np.mat(d) * f2w * np.mat(n).T)[0, 0]
+                w_para = np.linalg.norm(np.mat(d) * f2w * (np.identity(3) - np.mat(n).T * np.mat(n)))
+                if self.mu * w_perp > w_para:
+                    max_sw = (w_perp + w_para ** 2 / w_perp)
+                else:
+                    max_sw = max(0, w_perp + self.mu * w_para)
+                self.gij[i][j] = max_sw
+
     @staticmethod
     def cross(p):
-        ret=np.zeros((3,3),dtype=np.float64)
-        ret[2,1]= p[0]
-        ret[1,2]=-p[0]
-        ret[0,2]= p[1]
-        ret[2,0]=-p[1]
-        ret[1,0]= p[2]
-        ret[0,1]=-p[2]
+        ret = np.zeros((3, 3), dtype=np.float64)
+        ret[2, 1] = p[0]
+        ret[1, 2] = -p[0]
+        ret[0, 2] = p[1]
+        ret[2, 0] = -p[1]
+        ret[1, 0] = p[2]
+        ret[0, 1] = -p[2]
         return ret
 
     def draw_samples(self, scale, length):
         # show hand
         import vtk
-        from Hand import vtk_render,vtk_add_from_hand
+        from Hand import vtk_render, vtk_add_from_hand
         renderer = vtk.vtkRenderer()
         vtk_add_from_hand(self.meshes, renderer, 1.0, use_torch=True)
         vtk_add_metric_samples(renderer, self, scale, length)
         vtk_render(renderer, axes=True)
 
     def compute_exp_dist(self, link):
-        if isinstance(link,Hand):
+        if isinstance(link, Hand):
             return self.compute_exp_dist(link.palm)
         else:
-            trans = link.joint_transform_torch.numpy()[0,:,:]
-            pss = trans[:3,:3].T @ (self.points.T - trans[:3,3]).T
-            ret = [[link.hull.distance_to(pss[:,i]) for i in range(pss.shape[1])]]
+            trans = link.joint_transform_torch.numpy()[0, :, :]
+            pss = trans[:3, :3].T @ (self.points.T - trans[:3, 3]).T
+            ret = [[link.hull.distance_to(pss[:, i]) for i in range(pss.shape[1])]]
             if link.children:
                 for c in link.children:
                     ret += self.compute_exp_dist(c)
@@ -83,19 +85,21 @@ class Metric(object):
     def compute_metric(self, hand, alpha=.1):
         dists = self.compute_exp_dist(hand)
         dists_value = np.array([[d[0] for d in distsL] for distsL in dists])
-        exp_dists_max = np.amax(np.exp(dists_value * -alpha),axis=0)
-        Q_value = self.gij * np.expand_dims(exp_dists_max,axis=1)
-        Q_value = np.mean(np.amax(Q_value,axis=1),axis=0)
+        print(dists_value.shape)
+        exp_dists_max = np.amax(np.exp(dists_value * -alpha), axis=0)
+        Q_value = self.gij * np.expand_dims(exp_dists_max, axis=1)
+        Q_value = np.mean(np.amax(Q_value, axis=1), axis=0)
         return Q_value
+
 
 def vtk_add_metric_samples(renderer, metric, scale, length):
     import vtk
     for i in range(metric.points.shape[1]):
-        #point
+        # point
         sphere = vtk.vtkSphereSource()
-        sphere.SetCenter(metric.points[0,i],
-                         metric.points[1,i],
-                         metric.points[2,i])
+        sphere.SetCenter(metric.points[0, i],
+                         metric.points[1, i],
+                         metric.points[2, i])
         sphere.SetRadius(scale)
         sphere.SetThetaResolution(24)
         sphere.SetPhiResolution(24)
@@ -103,22 +107,22 @@ def vtk_add_metric_samples(renderer, metric, scale, length):
         sphere_mapper.SetInputConnection(sphere.GetOutputPort())
         sphere_actor = vtk.vtkActor()
         sphere_actor.SetMapper(sphere_mapper)
-        sphere_actor.GetProperty().SetColor(255./255,.0/255,255.0/255)
+        sphere_actor.GetProperty().SetColor(255. / 255, .0 / 255, 255.0 / 255)
         renderer.AddActor(sphere_actor)
 
-        #normal
-        if length<=0.:
+        # normal
+        if length <= 0.:
             continue
-        normal=vtk.vtkArrowSource()
+        normal = vtk.vtkArrowSource()
         normal.SetTipResolution(100)
         normal.SetShaftResolution(100)
         # Generate a random start and end point
-        startPoint=[metric.points[0,i],
-                    metric.points[1,i],
-                    metric.points[2,i]]
-        endPoint=[metric.points[0,i]+metric.normals[0,i]*length,
-                  metric.points[1,i]+metric.normals[1,i]*length,
-                  metric.points[2,i]+metric.normals[2,i]*length]
+        startPoint = [metric.points[0, i],
+                      metric.points[1, i],
+                      metric.points[2, i]]
+        endPoint = [metric.points[0, i] + metric.normals[0, i] * length,
+                    metric.points[1, i] + metric.normals[1, i] * length,
+                    metric.points[2, i] + metric.normals[2, i] * length]
         rng = vtk.vtkMinimalStandardRandomSequence()
         rng.SetSeed(8775070)  # For testing.
         # Compute a basis
@@ -130,7 +134,7 @@ def vtk_add_metric_samples(renderer, metric, scale, length):
         length = vtk.vtkMath.Norm(normalizedX)
         vtk.vtkMath.Normalize(normalizedX)
         # The Z axis is an arbitrary vector cross X
-        arbitrary=[0 for i in range(3)]
+        arbitrary = [0 for i in range(3)]
         for j in range(0, 3):
             rng.Next()
             arbitrary[j] = rng.GetRangeValue(-10, 10)
@@ -164,5 +168,5 @@ def vtk_add_metric_samples(renderer, metric, scale, length):
         else:
             normalMapper.SetInputConnection(transformPD.GetOutputPort())
         normalActor.SetMapper(normalMapper)
-        normalActor.GetProperty().SetColor(255.0/255, 0.0/255, 0.0/255)
+        normalActor.GetProperty().SetColor(255.0 / 255, 0.0 / 255, 0.0 / 255)
         renderer.AddActor(normalActor)
